68a282bbf5f1aed934aa1dd88eb322dcdd3b879a
Steven Truesdell <steven@strues.io>
2017-06-03 22:38:13 -0600

    fix: improve options input handling. enable for browser as well as node

--- browser.js
+++ browser.js
@@ -1,78 +1,77 @@
 const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
 
-const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-
-module.exports = {
-  presets: [
-    [
-      require.resolve('babel-preset-env'),
-      {
-        modules: false,
-        debug: false,
-        useBuiltIns: true,
-        targets: {
-          browsers: ['> 2% in US', 'last 2 versions'],
-        },
-        exclude: ['transform-regenerator', 'transform-async-to-generator'],
-      },
-    ],
-    require.resolve('babel-preset-react'),
-  ],
-  plugins: [
-    require.resolve('babel-plugin-syntax-flow'),
-    require.resolve('babel-plugin-syntax-dynamic-import'),
-    [
-      require.resolve('fast-async'),
-      {
-        spec: true,
-      },
-    ],
-    // class { handleClick = () => { } }
-    [
-      require.resolve('babel-plugin-transform-class-properties'),
-      {
-        spec: true,
-      },
-    ],
-    require.resolve('babel-plugin-transform-decorators-legacy'),
-    // { ...param, completed: true }
-    [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-    // Transforms JSX - Added so object-rest-spread in JSX uses builtIn
-    [
-      require.resolve('babel-plugin-transform-react-jsx'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-  ],
+const defaults = {
+  debug: false,
+  loose: false,
+  modules: false,
+  targets: {
+    uglify: true,
+    browsers: ['> .5% in US', 'last 1 versions'],
+  },
+  exclude: ['transform-regenerator', 'transform-async-to-generator'],
+  useBuiltIns: true
 };
 
-if (env === 'production') {
-  const prodPlugins = [
-    [
-      require.resolve('babel-plugin-transform-react-remove-prop-types'),
-      { removeImport: true },
+module.exports = function(context, opts = {}) {
+  const options = Object.assign({}, defaults, opts);
+
+  const config = {
+    presets: [
+      [require.resolve('babel-preset-env'), opts],
+      require.resolve('babel-preset-react'),
     ],
-    require.resolve('babel-plugin-transform-react-constant-elements'),
-    require.resolve('babel-plugin-transform-react-inline-elements'),
-    require.resolve('babel-plugin-transform-flow-strip-types'),
-  ];
+    plugins: [
+      require.resolve('babel-plugin-syntax-flow'),
+      require.resolve('babel-plugin-syntax-dynamic-import'),
+      [
+        require.resolve('fast-async'),
+        {
+          spec: true,
+        },
+      ],
+      // class { handleClick = () => { } }
+      [
+        require.resolve('babel-plugin-transform-class-properties'),
+        {
+          spec: true,
+        },
+      ],
+      require.resolve('babel-plugin-transform-decorators-legacy'),
+      // { ...param, completed: true }
+      [
+        require.resolve('babel-plugin-transform-object-rest-spread'),
+        {
+          useBuiltIns: true,
+        },
+      ],
+      // Transforms JSX - Added so object-rest-spread in JSX uses builtIn
+      [
+        require.resolve('babel-plugin-transform-react-jsx'),
+        {
+          useBuiltIns: true,
+        },
+      ],
+    ],
+  };
 
-  module.exports.plugins.push(...prodPlugins);
-}
+  if (process.env.NODE_ENV === 'development' || 'test') {
+    config.plugins.push(
+      // Adds component stack to warning messages
+      require.resolve('babel-plugin-transform-react-jsx-source'),
+      // Adds __self attribute to JSX which React will use for some warnings
+      require.resolve('babel-plugin-transform-react-jsx-self'),
+    );
+  }
 
-if (env === 'development' || env === 'test') {
-  const devPlugins = [
-    // Adds component stack to warning messages
-    require.resolve('babel-plugin-transform-react-jsx-source'),
-    // Adds __self attribute to JSX which React will use for some warnings
-    require.resolve('babel-plugin-transform-react-jsx-self'),
-  ];
+  if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+      [require.resolve('babel-plugin-transform-react-remove-prop-types'), { removeImport: true }],
+      require.resolve('babel-plugin-transform-react-constant-elements'),
+      require.resolve('babel-plugin-transform-react-inline-elements'),
+      require.resolve('babel-plugin-transform-flow-strip-types'),
+    );
+  }
 
-  module.exports.plugins.push(...devPlugins);
+  return config;
 }
--- node.js
+++ node.js
@@ -1,71 +1,53 @@
 const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
 
-const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-const validTargets = ['7', '7.6', '8', 'current'];
-
-module.exports = function(context, opts) {
-  opts = opts || { };
-  const targetOption = String(opts.target);
-  // use indexOf to support node 4
-  if (targetOption && validTargets.indexOf(targetOption) === -1) {
-    throw new Error(`Preset boldr/node 'target' option must one of ${validTargets.join(', ')}.`);
-  }
-
-  const target = targetOption !== 'current' ? targetOption : process.versions.node;
-  const loose = opts.loose !== undefined ? opts.loose : false;
-  const modules = opts['preset-env'].modules !== undefined ? opts['preset-env'].modules : 'commonjs';
-
-  if (modules !== false && modules !== 'commonjs') {
-    throw new Error("Preset boldr/node 'modules' option must be 'false' to indicate no modules\n" +
-      "or 'commonjs' (default)");
-  }
-  if (typeof loose !== 'boolean') throw new Error("Preset boldr/node 'loose' option must be a boolean.");
-  const optsLoose = { loose };
-
-  const envOpts = {
-    debug: false,
-    modules: modules,
-  	targets: {
-      node: target
-    },
-  	useBuiltIns: true,
-    exclude: ['transform-regenerator', 'transform-async-to-generator'],
-  }
-  presets: [
-    [
-      require.resolve('babel-preset-env'),
-      Object.assign({ }, envOpts, opts['preset-env'] || { })
-    ],
-    require.resolve('babel-preset-react')
-  ],
-  plugins: [
-    require.resolve('babel-plugin-syntax-flow'),
-    require.resolve('babel-plugin-syntax-dynamic-import'),
-    [
-      require.resolve('fast-async'),
-      {
-        spec: true,
-      },
-    ],
-    modules === 'commonjs' && [require('babel-plugin-transform-es2015-modules-commonjs'), optsLoose],
-    // class { handleClick = () => { } }
-    require.resolve('babel-plugin-transform-class-properties'),
-    // { ...param, completed: true }
-    [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-    require.resolve('babel-plugin-transform-decorators-legacy'),
-
-    require.resolve('babel-plugin-dynamic-import-node'),
-  ],
+const defaults = {
+  debug: false,
+  loose: false,
+  modules: false,
+  targets: {
+    node: 8
+  },
+  exclude: ['transform-regenerator', 'transform-async-to-generator'],
+  useBuiltIns: true
 };
-if (env === 'production') {
-  const prodPlugins = [
-    require.resolve('babel-plugin-transform-flow-strip-types'),
-  ];
 
-  module.exports.plugins.push(...prodPlugins);
+module.exports = function(context, opts = {}) {
+  const options = Object.assign({}, defaults, opts);
+
+  const config = {
+    presets: [
+      [require.resolve('babel-preset-env'), opts],
+      require.resolve('babel-preset-react'),
+    ],
+    plugins: [
+      require.resolve('babel-plugin-syntax-flow'),
+      require.resolve('babel-plugin-syntax-dynamic-import'),
+      [
+        require.resolve('fast-async'),
+        {
+          spec: true,
+        },
+      ],
+      // class { handleClick = () => { } }
+      require.resolve('babel-plugin-transform-class-properties'),
+      // { ...param, completed: true }
+      [
+        require.resolve('babel-plugin-transform-object-rest-spread'),
+        {
+          useBuiltIns: true,
+        },
+      ],
+      require.resolve('babel-plugin-transform-decorators-legacy'),
+
+      require.resolve('babel-plugin-dynamic-import-node'),
+    ],
+  };
+  if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+      require.resolve('babel-plugin-transform-flow-strip-types')
+    );
+  }
+
+  return config;
 }
